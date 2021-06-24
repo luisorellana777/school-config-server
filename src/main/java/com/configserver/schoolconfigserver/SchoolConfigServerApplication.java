b2ecf532f7c7fb9de7dae8fe52d012d8e00d88f9
Luis Orellana Altamirano <luis.orellana.a@usach.cl>
2021-06-24 14:27:58 -0400

    initial Commit

---
+++ src/main/java/com/configserver/schoolconfigserver/SchoolConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.configserver.schoolconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SchoolConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/configserver/schoolconfigserver/SchoolConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configserver.schoolconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
